8396b3395cafff62e651b3dfaf06687ef490e64c
ElChrisVazquez <thechrisvazquez@gmail.com>
2024-10-25 14:32:31 -0600

    03_custom-blocks

--- src/main/java/net/thechrisvazquez/tutorialmod/TutorialMod.java
+++ src/main/java/net/thechrisvazquez/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.thechrisvazquez.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.thechrisvazquez.tutorialmod.block.ModBlocks;
 import net.thechrisvazquez.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
     }
 }
