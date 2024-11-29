e7c64049d167e966516d1400c51053d83a9f992d
ElChrisVazquez <thechrisvazquez@gmail.com>
2024-11-29 11:27:39 -0600

    Se añade reloj

--- src/main/java/net/thechrisvazquez/tutorialmod/item/ModItems.java
+++ src/main/java/net/thechrisvazquez/tutorialmod/item/ModItems.java
@@ -11,6 +11,7 @@ import net.thechrisvazquez.tutorialmod.TutorialMod;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item TELEPORT_CLOCK = registerItem("teleport_clock", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -22,6 +23,7 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(TELEPORT_CLOCK);
         });
     }
 }
